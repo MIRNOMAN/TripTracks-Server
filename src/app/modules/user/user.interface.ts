7ff827fa82338e23145dab53e2a5f1a7879d43f1
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-20 23:20:37 +0600

    booking, flloewr create

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -14,3 +14,34 @@ export type TUser = {
   following: Types.ObjectId[] // Use array of ObjectIds directly without wrapping in an object
   followers: Types.ObjectId[] // Same for followers
 }
+
+
+export type TFollow = {
+    userId: Types.ObjectId
+    targetedId: Types.ObjectId
+  }
+  
+  export interface UserModel extends Model<TUser> {
+    //instance methods for checking if the user exist
+    isUserExistsByEmail(email: string): Promise<TUser>
+    //instance methods for checking if passwords are matched
+    isPasswordMatched(
+      plainTextPassword: string,
+      hashedPassword: string,
+    ): Promise<boolean>
+  }
+  
+  export type TUserRole = keyof typeof USER_ROLE
+  
+  export interface IBooking {
+    _id: string
+    customer: string // User ID
+    vehicleType: string
+    vehicleBrand: string
+    vehicleModel: string
+    manufacturingYear: number
+    registrationPlate: string
+    createdAt: string // ISO 8601 date string
+    updatedAt: string // ISO 8601 date string
+    __v: number
+  }
