e4f8d43020f8699d82135cf69eb3e218e107b657
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-21 00:10:58 +0600

    create update userInto db

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -5,3 +5,11 @@ const createUserIntoDb = async (userData: TUser) => {
     const result = await User.create(userData)
     return result
   }
+
+  const updateUserIntoDB = async (id: string, payload: Partial<TUser>) => {
+    const result = await User.findByIdAndUpdate(id, payload, {
+      new: true,
+      runValidators: true,
+    })
+    return result
+  }
