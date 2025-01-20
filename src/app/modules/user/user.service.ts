6d761cb4f2ae3c66e973fd4ebfb05deadce84c22
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-21 00:17:48 +0600

    create my booking from DB

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import { User } from "./user.model"
 
 
 
@@ -39,4 +40,25 @@ const createUserIntoDb = async (userData: TUser) => {
       throw new Error('User not found!')
     }
     return result
+  }
+
+  const getMyBookingsFromDb = async (email: string) => {
+    // const result = await Booking.find().populate('customer')
+    const user = await User.findOne({ email })
+    if (user) {
+      const result = await Booking.find({
+        customer: user._id,
+        status: { $ne: 'pending' },
+      })
+        .populate('service', '_id name description price duration')
+        .populate('slot', '_id service date startTime endTime isBooked')
+        .select('-customer')
+        .sort({ createdAt: -1 })
+        .lean()
+      return result
+    }
+  }
+  type FollowPayload = {
+    userId: string // Assuming you're passing userId and targetedId as strings
+    targetedId: string
   }
