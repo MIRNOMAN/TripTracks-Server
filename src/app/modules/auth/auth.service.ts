f20364830608a18fdcdec78ae03242cd37d9fdd1
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-22 01:52:07 +0600

    create login user

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -22,3 +22,33 @@ export const generateToken = (user: TUser) => {
   )
   return accessToken
 }
+
+const loginUser = async (payload: TLoginUser) => {
+    // checking if the user is exist
+    const user = await User.isUserExistsByEmail(payload.email)
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !')
+    }
+    //checking if the password is correct
+    if (!(await User.isPasswordMatched(payload?.password, user?.password)))
+      throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched')
+    //create token and sent to the  client
+    const jwtPayload = {
+      email: user.email,
+      role: user.role,
+      status: user.status,
+      id: user._id,
+    }
+  
+    const accessToken = createToken(
+      jwtPayload,
+      config.jwt_access_secret as string,
+      config.jwt_access_expires_in as string,
+    )
+  
+    return {
+      accessToken: accessToken,
+      user,
+    }
+  }
+  
