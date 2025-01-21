22855dd6a73090499337e6a2be546b8e1cc07ff5
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-22 01:55:20 +0600

    create refresh token controller

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -44,3 +44,21 @@ const changePassword = catchAsync(async (req, res) => {
       data: result,
     })
   }) 
+  const getRefreshToken = catchAsync(async (req, res) => {
+    const token = req.headers.authorization
+    const { id } = getUserInfoFromToken(token as string)
+    const result = await AuthServices.refreshToken(id)
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Refresh token generated successfully',
+      data: result,
+    })
+  })
+  
+  export const AuthControllers = {
+    loginUser,
+    passwordRecover,
+    changePassword,
+    getRefreshToken,
+  }
