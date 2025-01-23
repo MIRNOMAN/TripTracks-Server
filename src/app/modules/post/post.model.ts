6fb2b47cc346a07b3ac9e754f2d2a67373c8f7d8
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-24 01:10:11 +0600

    create post schema model

--- src/app/modules/post/post.model.ts
+++ src/app/modules/post/post.model.ts
@@ -11,3 +11,43 @@ const CommentSchema: Schema = new Schema<TComment>(
     timestamps: true,
   },
 )
+
+const PostSchema: Schema = new Schema<TPost>(
+    {
+      title: { type: String, required: true },
+      content: { type: String, required: true },
+      cover: { type: String, required: true },
+      tags: { type: String, enum: ['premium', 'regular'], required: true },
+      upVotes: [{ type: Schema.Types.ObjectId, ref: 'user', required: true }],
+      downVotes: [{ type: Schema.Types.ObjectId, ref: 'user', required: true }],
+      comments: [CommentSchema],
+      commentsCount: { type: Number, default: 0 },
+      author: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+      category: {
+        type: String,
+        enum: [
+          'adventure',
+          'eco-tourism',
+          'luxury',
+          'wellness',
+          'cultural',
+          'culinary',
+          'historical',
+          'beach',
+          'mountain',
+          'road trip',
+          'travel',
+        ],
+        default: 'travel',
+      },
+      isActive: { type: Boolean, default: true },
+    },
+    {
+      timestamps: true,
+    },
+  )
+  
+  // Create the Post model
+  const Post = mongoose.model<TPost>('Post', PostSchema)
+  
+  export default Post
