d3d175745d2c583ab9a96891dfb4f2277ceec5f2
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-24 01:22:17 +0600

    create createPost controller

---
+++ src/app/modules/post/post.controller.ts
@@ -0,0 +1,27 @@
+import httpStatus from 'http-status'
+import sendResponse from '../../utils/sendResponse'
+import catchAsync from '../../utils/catchAsync'
+import { TPost } from './post.interface'
+import { TImageFile } from '../../interface/image.interface'
+import { postServices } from './post.service'
+import Post from './post.model'
+import { getUserInfoFromToken } from '../../utils/getUserInfoFromToken'
+
+const createPost = catchAsync(async (req, res) => {
+  const postInfo = req.body
+  const file = req.file as TImageFile
+  const imagePath = file?.path
+  const payload = {
+    ...postInfo,
+    cover: imagePath,
+  }
+
+  const result = await postServices.createPostIntoDB(payload)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Post created successfully',
+    data: result,
+  })
+})
