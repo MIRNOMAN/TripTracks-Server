eb1ddf3c0dce10a35493da9ca386bb668aff60ba
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-24 01:27:06 +0600

    create getPopularPosts controller

--- src/app/modules/post/post.controller.ts
+++ src/app/modules/post/post.controller.ts
@@ -82,3 +82,107 @@ const updatePost = catchAsync(async (req, res) => {
       data: result,
     })
   })
+
+  const getPopularPosts = catchAsync(async (req, res) => {
+    const result = await Post.aggregate([
+      {
+        $addFields: {
+          upVotesCount: { $size: '$upVotes' }, // Add a field to calculate the length of the upVotes array
+        },
+      },
+      {
+        $sort: { upVotesCount: -1 }, // Sort by the length of the upVotes array in descending order
+      },
+      {
+        $limit: 3,
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'author',
+          foreignField: '_id',
+          as: 'author',
+        },
+      },
+      {
+        $unwind: '$author',
+      },
+      {
+        $project: {
+          images: 0,
+          downVotes: 0,
+          commentsCount: 0,
+          category: 0,
+          comments: 0,
+          'author.email': 0,
+          'author.followers': 0,
+          __v: 0,
+        },
+      },
+    ])
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Top 3 posts retrieved successfully',
+      data: result,
+    })
+  })
+  const getSinglePost = catchAsync(async (req, res) => {
+    const { id } = req.params
+    const result = await postServices.getSinglePostFromDB(id)
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Post retrieved successfully',
+      data: result,
+    })
+  })
+  const deletePost = catchAsync(async (req, res) => {
+    const { id } = req.params
+    const result = await Post.findByIdAndDelete(id)
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Post deleted successfully',
+      data: result,
+    })
+  })
+  const upVotePost = catchAsync(async (req, res) => {
+    const { id } = req.params
+    const token = req.headers.authorization
+    const { id: userId } = getUserInfoFromToken(token as string)
+    const result = await postServices.upVotePostIntoDB(id, userId)
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Voted successfully',
+      data: result,
+    })
+  })
+  const downVotePost = catchAsync(async (req, res) => {
+    const { id } = req.params
+    const token = req.headers.authorization
+    const { id: userId } = getUserInfoFromToken(token as string)
+    const result = await postServices.downVotePostIntoDB(id, userId)
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Down voted successfully',
+      data: result,
+    })
+  })
+  
+  export const postControllers = {
+    createPost,
+    getAllPosts,
+    getAllAcInacPosts,
+    getPostsByAuthor,
+    getPopularPosts,
+    getSinglePost,
+    updatePost,
+    deletePost,
+    upVotePost,
+    downVotePost,
+  }
