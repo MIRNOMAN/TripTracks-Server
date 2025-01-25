8fcdd62288bafb7a98b044427f3165eaddef99a2
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-25 22:28:59 +0600

    create comment model

--- src/app/modules/comment/comment.service.ts
+++ src/app/modules/comment/comment.service.ts
@@ -1,7 +1,18 @@
-import { Types } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+import { TComment } from './comment.interface'
 
-export type TComment = {
-  postId: Types.ObjectId
-  userId: Types.ObjectId
-  feedback: string
-}
+const commentSchema = new Schema<TComment>(
+  {
+    postId: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    feedback: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+// Create the Comment model
+const Comment = mongoose.model<TComment>('Comment', commentSchema)
+
+export default Comment
