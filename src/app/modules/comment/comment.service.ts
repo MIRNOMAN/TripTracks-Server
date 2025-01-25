a09cc3f51fc36378df6c71b00775a076c864a0cf
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-25 22:30:33 +0600

    create EditCommentIntoPost service

--- src/app/modules/comment/comment.service.ts
+++ src/app/modules/comment/comment.service.ts
@@ -18,3 +18,34 @@ const commentIntoPost = async (id: string, payload: TComment) => {
 
   return result
 }
+
+
+const EditCommentIntoPost = async (id: string, payload: TComment) => {
+  const { userId, feedback } = payload
+
+  const isFeedbackAvailable = await Comment.findById(id)
+
+  if (!isFeedbackAvailable) {
+    throw new Error('Comment not found')
+  }
+  // Compare userId with isFeedbackAvailable.userId
+  const isSameUser = new Types.ObjectId(userId).equals(
+    isFeedbackAvailable.userId,
+  )
+
+  if (!isSameUser) {
+    throw new Error('Unauthorized: You can only edit your own comments')
+  }
+
+  const result = await Comment.findByIdAndUpdate(
+    id,
+    {
+      feedback,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+  return result
+}
